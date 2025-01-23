326be85488a8a08e5f2c0b4d9bc43848e983a2e3
giangallino <99291206+gianlucaegallino@users.noreply.github.com>
2025-01-23 14:27:18 -0300

    Finish styling shop section

--- src/components/ShopSidebar.jsx
+++ src/components/ShopSidebar.jsx
@@ -1,4 +1,45 @@
-import s from "../styles/ShopSidebar.module.css"
-export default function ShopSidebar(){
-    return(<div className={s.sidebar}> side </div>)
+
+import s from "../styles/ShopSidebar.module.css";
+import ItemList from "./ItemList";
+import { Link } from "react-router-dom";
+export default function ShopSidebar() {
+  return (
+    <div className={s.sidebar}>
+      <div className={s.container}>
+        <h2 className={s.title}>Selected Items</h2>
+        <ItemList
+          items={[
+            {
+              name: "Green Pendant Necklace",
+              amount: 1,
+              price: 25.99,
+              key: "gp",
+            },
+            {
+              name: "Black Cotton T-Shirt",
+              amount: 2,
+              price: 15.49,
+              key: "bc",
+            },
+            {
+              name: "Gold Ring",
+              amount: 1,
+              price: 45.0,
+              key: "gr",
+            },
+            {
+              name: "Nature Print Hoodie",
+              amount: 1,
+              price: 35.99,
+              key: "np",
+            },
+          ]}
+        />
+      </div>
+      <Link className={s.li} to="/shop">
+        {" "}
+        <button className={s.button}> Go to Cart</button>
+      </Link>
+    </div>
+  );
 }
